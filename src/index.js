7ba3da8630511549de6a039d415d08dcce16ab31
Zakaria Mansouri <ZAQO.cs@outlook.com>
2018-03-16 21:02:48 +0100

    added 404 page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Redirect, BrowserRouter as Router, Route } from 'react-router-dom'
+import { Switch, Redirect, BrowserRouter as Router, Route } from 'react-router-dom'
 import AsyncComponent from './components/AsyncComponent'
 
 const homePage = () => import(/* webpackChunkName: "home" */ './scenes/home')
 const profilePage = () => import(/* webpackChunkName: "profile" */ './scenes/profile')
 const authPage = () => import(/* webpackChunkName: "auth" */ './scenes/auth')
 const joinPage = () => import(/* webpackChunkName: "join" */ './scenes/join')
+const notFoundPage = () => import(/* webpackChunkName: "not-found" */ './scenes/not-found')
 
 var auth = {
   isAuthenticated: false,
@@ -15,11 +16,14 @@ var auth = {
 ReactDOM.render(
   <Router>
     <div>
-      <Route exact path='/' component={() => <AsyncComponent moduleProvider={auth.isAuthenticated ? homePage : authPage} />} />
-      <Route exact path='/Profile' component={auth.isAuthenticated ?
-        () => <AsyncComponent moduleProvider={profilePage} /> : () => <Redirect to='/' />} />
-      <Route exact path='/Join' component={!auth.isAuthenticated ?
-        () => <AsyncComponent moduleProvider={joinPage} /> : () => <Redirect to='/' />} />
+      <Switch>
+        <Route exact path='/' component={() => <AsyncComponent moduleProvider={auth.isAuthenticated ? homePage : authPage} />} />
+        <Route exact path='/Profile' component={auth.isAuthenticated ?
+          () => <AsyncComponent moduleProvider={profilePage} /> : () => <Redirect to='/' />} />
+        <Route exact path='/Join' component={!auth.isAuthenticated ?
+          () => <AsyncComponent moduleProvider={joinPage} /> : () => <Redirect to='/' />} />
+        <Route component={() => <AsyncComponent moduleProvider={notFoundPage} />} />
+      </Switch>
     </div>
   </Router>
   ,
