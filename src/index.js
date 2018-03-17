52890a58a0e89cf1b1c3806f22145d7969e48fae
Zakaria Mansouri <ZAQO.cs@outlook.com>
2018-03-17 11:27:35 +0100

    added a loading component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Switch, Redirect, BrowserRouter as Router, Route } from 'react-router-dom'
-import AsyncComponent from './components/AsyncComponent'
+import AsyncComponent from './components/AsyncComponent/'
+import LoadingMain from './components/loading-main/'
 
 const homePage = () => import(/* webpackChunkName: "home" */ './scenes/home')
 const profilePage = () => import(/* webpackChunkName: "profile" */ './scenes/profile')
@@ -17,12 +18,12 @@ ReactDOM.render(
   <Router>
     <div>
       <Switch>
-        <Route exact path='/' component={() => <AsyncComponent moduleProvider={auth.isAuthenticated ? homePage : authPage} />} />
+        <Route exact path='/' component={() => <AsyncComponent loadingComponent={LoadingMain} moduleProvider={auth.isAuthenticated ? homePage : authPage} />} />
         <Route exact path='/Profile' component={auth.isAuthenticated ?
-          () => <AsyncComponent moduleProvider={profilePage} /> : () => <Redirect to='/' />} />
+          () => <AsyncComponent loadingComponent={LoadingMain} moduleProvider={profilePage} /> : () => <Redirect to='/' />} />
         <Route exact path='/Join' component={!auth.isAuthenticated ?
-          () => <AsyncComponent moduleProvider={joinPage} /> : () => <Redirect to='/' />} />
-        <Route component={() => <AsyncComponent moduleProvider={notFoundPage} />} />
+          () => <AsyncComponent loadingComponent={LoadingMain} moduleProvider={joinPage} /> : () => <Redirect to='/' />} />
+        <Route component={() => <AsyncComponent loadingComponent={LoadingMain} moduleProvider={notFoundPage} />} />
       </Switch>
     </div>
   </Router>
