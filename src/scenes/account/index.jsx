d8cca0305e47259fc7c5ed5ce177fd599e50e655
Zakaria Mansouri <ZAQO.cs@outlook.com>
2018-03-19 23:28:37 +0100

    some renamings

--- src/scenes/account/index.jsx
+++ src/scenes/account/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const profilePage = () => (
+const accountPage = () => (
   <div>
     <h1>{'{|02 page title|}'}</h1>
     <h2>{'Here\'s you profile info :)'}</h2>
   </div>
 )
 
-export const Component = profilePage
+export const Component = accountPage
